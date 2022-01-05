b9b393354b02379484efbb01adb1100284eefec4
Kishore G <kpgpivotal@gmail.com>
2022-01-05 18:45:14 +0530

    Add vieset API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views       import APIView
 from rest_framework.response    import Response
 from rest_framework             import status
+from rest_framework             import viewsets
 
 from profiles_api               import serializers
 
@@ -47,3 +48,17 @@ class HelloAPIView(APIView):
         """ Delete   an object  """
 
         return Response({'method': 'DELETE'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """ Test APIViewset """
+
+    def list(self, request):
+        """ Retutn Helll message """
+
+        a_viewset = ['Uses actions (list, create, retrieve, update, partial_update, delete)',
+            'Automatically maps to URLs using Routers',
+            ' Provides more functionality with less code'
+            ]
+
+        return Response({'message': 'Hello', 'a_viewset':a_viewset })
